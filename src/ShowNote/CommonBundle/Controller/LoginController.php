e65070e62482315699a57e1cd7849dc30e057a85
Eric Johnson <megamandos@gmail.com>
2014-07-04 19:28:04 -0400

    Added stubs for the login page.

---
+++ src/ShowNote/CommonBundle/Controller/LoginController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace ShowNote\CommonBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class DefaultController extends Controller
+{
+    public function loginAction(Request $request)
+    {
+        return $this->render('ShowNoteCommonBundle:Login:login.html.twig');
+    }
+}
